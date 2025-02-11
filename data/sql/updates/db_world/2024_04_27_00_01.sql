80e61d49e4b4ecd6a357ef327d319550ff63b7f0
grimgravy <sr.alansantos@hotmail.com>
2025-02-11 09:26:51 -0300

    Update Data

--- data/sql/updates/db_world/2024_04_27_00_01.sql
+++ data/sql/updates/db_world/2024_04_27_00_01.sql
@@ -1,4 +1,4 @@
--- update 2024_04_26_00_00 -> 2024_04_26_00_01
+-- update 2024_04_27_00_00 -> 2024_04_27_00_01
 DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_gen_dest_caster_summon';
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (32205, 'spell_gen_dest_caster_summon'), -- Place Mag'har Battle Standard
