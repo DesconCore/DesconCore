2bca8b0c2882cc85eaa1fb082aa6a7ac0b3b6168
grimgravy <sr.alansantos@hotmail.com>
2025-02-11 11:33:25 -0300

    fix(core/form flight): item Enchant Gloves - Riding Skill, Mithril Spurs and Carrot on a Stick.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -14320,7 +14320,8 @@ void Unit::UpdateSpeed(UnitMoveType mtype, bool forced)
                 {
                     main_speed_mod  = GetTotalAuraModifier(SPELL_AURA_MOD_INCREASE_FLIGHT_SPEED) + GetTotalAuraModifier(SPELL_AURA_MOD_INCREASE_VEHICLE_FLIGHT_SPEED);
 
-                    if (GetShapeshiftForm() != FORM_FLIGHT_EPIC || GetShapeshiftForm() != FORM_FLIGHT)
+                    // Druid Form: Enchant gloves, hand, boots and trinket (Carrot on a Stick)
+                    if (GetShapeshiftForm() == FORM_FLIGHT_EPIC || GetShapeshiftForm() == FORM_FLIGHT)
                         stack_bonus = GetTotalAuraMultiplier(SPELL_AURA_MOD_MOUNTED_FLIGHT_SPEED_ALWAYS);
                 }
 
